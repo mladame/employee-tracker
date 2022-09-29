c06c34b298c5b5155e02006866a09a674c403ec0
Martha A <mladame43@gmail.com>
2022-09-28 22:46:38 -0700

    added schema, seeds, missing manger_id in seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,33 @@
+-- delete and create database
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db
+
 -- create tables
 -- set ids to auto increment
 
 -- department
 -- id - int - primary key
 -- name
+CREATE TABLE department (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
 
 -- role
 -- id
 -- title
 -- salary
 -- department id - foreign key - dept. table id
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT, 
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+)
 
 -- employee
 -- id
@@ -17,3 +35,11 @@
 -- last name
 -- role id - foreign key - role table id
 -- manager id - reference employee id
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name:VARCHAR(30) NOT NULL,
+    last_name:VARCHAR(30) NOT NULL,
+    role_id: INT,
+    manager_id: INT,
+
+)
--- db/seeds.sql
+++ db/seeds.sql
@@ -3,9 +3,36 @@
 
 -- department
 -- name
+INSERT INTO department (name)
+VALUES  ("Sales & Marketing"),
+        ("Accouting & Finance"),
+        ("Research & Development"),
+        ("Administration");
 
 -- role
 -- title, salary, dept. id
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Salesman", 50000, 1),
+        ("Marketing Coordinator", 60000, 1),
+        ("Accountant", 80000, 2),
+        ("Finance Planner" 85000, 2),
+        ("Software Engineer", 100000, 3),
+        ("Research Scientist", 90000, 3),
+        ("Data Entry Clerk", 40000, 4),
+        ("Executive Assistant" 50000, 4)
 
 -- employee
 -- first, last, role id, manager id
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('Paulina', 'Mendoza', 1, ),
+        ('Krishna', 'Shea', 1, ),
+        ('Ben', 'Brook', 2, ),
+        ('Mila', 'Abello', 4, ),
+        ('Andrew', 'Taylor',3, ),
+        ('Maya', 'Ivanov', 3, ),
+        ('Bryon', 'Gorbold', 5, ),
+        ('Jolie', 'Terry', 5, ),
+        ('Darnell', 'Blake', 6, ),
+        ('Cleo', 'Power', 8, ),
+        ('Eddie', 'Stafford', 7, ),
+        ('Kurtis', 'Patton', 7, );
