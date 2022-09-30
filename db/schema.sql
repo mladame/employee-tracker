3be52d77cf8c4752d9fec792fb35482ed6707759
Martha A <mladame43@gmail.com>
2022-09-29 23:05:20 -0700

    updated manager reference, added delete/update constraints

--- db/schema.sql
+++ db/schema.sql
@@ -18,13 +18,19 @@ CREATE TABLE role (
     department_id INT, 
     FOREIGN KEY (department_id)
     REFERENCES department(id)
-)
+);
 
+-- add foreign key and reference
 CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name:VARCHAR(30) NOT NULL,
     last_name:VARCHAR(30) NOT NULL,
     role_id: INT,
     manager_id: INT,
-
-)
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
+);
